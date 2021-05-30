58f5bd2254240900b5e6f7ba1b99b0819b39336f
vdb5699 <nikhil.p0936@gmail.com>
2021-05-30 22:48:28 +1200

    Updating Layers
    Nikhil Updating Layers

--- FreeFormSurface/Layers.m
+++ FreeFormSurface/Layers.m
@@ -8,6 +8,7 @@ classdef Layers
         Px;
         Py;
         Pz;
+        type;
         
         
         %        BxRows;
@@ -23,14 +24,16 @@ classdef Layers
     end
     %% constructor
     methods(Access = public)
-        function obj = Layers(surface, thickness, numOfLayers)
+        function obj = Layers(surface, thickness, numOfLayers, type)
             points = surface.data;
             obj.Px = points.Px;
             obj.Py = points.Py;
             obj.Pz = points.Pz;
             obj.thickness = thickness;     %Need to get user input here
+            obj.type = type;
             obj.numOfLayers = numOfLayers;
             zPlusOne(obj, obj.Px,obj.Py, obj.Pz, obj.thickness);
+            %LayerCoordinates(obj, obj.Px, obj.Py, obj.Pz, obj.thickness);
         end
     end
     %% Adding 1 to Z value (Easy Way Out)
@@ -39,68 +42,99 @@ classdef Layers
             figure(10);
             surf(Px,Py,Pz);
             hold on;
-            title('Bezier Surface');
+            title('Surface with Layers');
             grid on;
             xlabel('X-Axis');
             ylabel('Y-Axis');
             zlabel('Z-Axis');
-            for row = 1:obj.numOfLayers
-                PzTemp = Pz + (thickness * row);
-                surf(Px,Py,PzTemp);
-                
-                
-                %                 [PzRows,PzCols] = size(Pz);
-                %                 oneMatrix = ones(PzRows, PzCols);
-                %                 Pz = Pz + oneMatrix + thickness;
-                %T = table(Bx, By, Bz);
+            
+            figure(11);
+            plot3(Px,Py,Pz);
+            hold on
+            title('Cutter Paths with Layers');
+            xlabel('X-Axis');
+            ylabel('Y-Axis');
+            zlabel('Z-Axis');
+            if obj.type == 1
+                for row = 1:obj.numOfLayers
+                    PzTemp = Pz + (thickness * row);
+                    figure(10);
+                    surf(Px,Py,PzTemp);
+    %                 plot3(Px,Py,Pz);
+                end
+            else
+                for row = 1:obj.numOfLayers
+                    PzTemp = Pz + (thickness * row);
+                    figure(11);
+                    plot3(Px,Py,PzTemp);
+                end
             end
         end
     end
     %% putting x,y,z values together in an array and calculating vectors
     methods (Access = public)
         function V5 = LayerCoordinates(obj, Px, Py, Pz, thickness)
-            [BxRows,BxCols] = size(Bx);
-            [ByRows,ByCols] = size(By);
-            [BzRows,BzCols] = size(Bz);
+            [PxRows,PxCols] = size(Px);
+            [PyRows,PyCols] = size(Py);
+            [PzRows,PzCols] = size(Pz);
             
-            for i=1:BxRows
-                for n=1:BxCols
-                    
-                    A = [Px(i,n), Py(i,n), Pz(i,n)];             %Point1
-                    if(n+1) <= BxCols
-                        B = [Bx(i,n+1), By(i,n+1), Bz(i,n+1)];   %Point2
+            for layers = 1:obj.numOfLayers
+                Px1 = [];
+                Py1 = [];
+                Pz1 = [];
+                for i=1:PxRows
+                    for n=1:PxCols
+                        A = [Px(i,n) Py(i,n) Pz(i,n)];             %Point1
+                        if(n+1) <= PxCols %next P-coordinates
+                            B = [Px(i,n+1) Py(i,n+1) Pz(i,n+1)];   %Point2
+%                         else
+%                             B = [0 0 0];
+                        end
+                        if(n+2) <= PxCols%next P-coordinates
+                            C = [Px(i,n+2) Py(i,n+2) Pz(i,n+2)];   %Point3
+%                         else
+%                             C = [0 0 0];
+                        end 
+                        V1 = A - B;
+                        V2 = C - B;
+                        V3 = cross(V2, V1);
+                        V1crossV3 = cross(V1, V3);
+                        V3crossV2 = cross(V3,V2);
+                        magnitude = norm(V1crossV3);                  %Calculating magnitude of V3
+                        V13 = obj.thickness * V1crossV3/magnitude;   %Unit Vector
+                        magnitude2 = norm(V3crossV2);           %Calculating magnitude of V3crossV2
+                        V23 = obj.thickness * V3crossV2/magnitude2;   %Unit Vector
+                        alpha = vectorAng(obj, V13,V23);
+                        V5 = (obj.thickness/cos(alpha/2))*((V13+V23)/norm(V13+V23));
+                        Px1(i,n) = obj.Px(i,n) + V5(1);
+                        Py1(i,n) = obj.Py(i,n) + V5(2);
+                        Pz1(i,n) = obj.Pz(i,n) + V5(3);
                     end
-                    if(n+2) <= BxCols
-                        C = [Bx(i,n+2), By(i,n+2), Bz(i,n+2)];   %Point3
-                    end
-                    V1 = A - B;
-                    V2 = C - B;
-                    V3 = cross(V2, V1);
-                    V1crossV3 = cross(V1, V3);
-                    V3crossV2 = cross(V3,V2);
-                    
-                    magnitude = vectorMag(V3);               %Calculating magnitude of V3
-                    V13 = obj.thickness * V1crossV3/magnitude;   %Unit Vector
-                    magnitude = vectorMag(V3crossV2);        %Calculating magnitude of V3crossV2
-                    V23 = obj.thickness * V3crossV2/magnitude;   %Unit Vector
-                    alpha = alphaAngle(V13,V23);
-                    V5 = (obj.thickness/cos(alpha/2))*((V13+V23)/norm(V13+V23));
+                end
+            
+                if obj.type == 1
+                    figure(10);
+                    surf(Px1, Py1, Pz1);
+                else
+                    figure(11);
+                    plot3(Px1, Py1, Pz1);
                 end
             end
         end
     end
-    %% Calculate Magnitude of Vectors
-    methods (Access = public)
-        function mag = vectorMag(v)      %takes input vector as parameter
-            dotProd = dot(v,v);          % dot product
-            mag = norm(dotProd);        % magnitude
-        end
-    end
+%     %% Calculate Magnitude of Vectors
+%     methods (Access = public)
+%         function mag = vectorMag(obj, v)      %takes input vector as parameter
+%             dotProd = dot(v,v);          % dot product
+%             mag = norm(dotProd);        % magnitude
+%             return;
+%         end
+%     end
     %% Calculate Alpha Angle between 2 vectors
     methods (Access = public)
-        function alphaAngle = vectorAng(v,v2)      %takes input vector v and v2 as parameter
-            radAngle = atan2(norm(cross(v,v2)), dot(v,v2));  %gets the angle between two vectors in rads
-            alphaAngle = rad2deg(radAngle);        %returns the angle in deg
+        function alphaAngle = vectorAng(obj, v,v2)      %takes input vector v and v2 as parameter
+            alphaAngle = atan2(norm(cross(v,v2)), dot(v,v2));  %gets the angle between two vectors in rads
+            %alphaAngle = rad2deg(radAngle);        %returns the angle in deg
         end
     end
     
